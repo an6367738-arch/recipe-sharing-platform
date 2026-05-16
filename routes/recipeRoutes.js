db415b6338c5c2aaf36b7465389a196bd2f7e3a4
AKANKSHA <an6367738@gmail.com>
2026-05-16 15:50:34 +0530

    Completed Week 5 search functionality

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const router = express.Router();
 
+const recipes = [
+  {
+    id: 1,
+    title: "Paneer Recipe",
+    category: "Dinner"
+  },
+  {
+    id: 2,
+    title: "Pasta",
+    category: "Lunch"
+  },
+  {
+    id: 3,
+    title: "Burger",
+    category: "Fast Food"
+  }
+];
+
+// Get all recipes
 router.get("/", (req, res) => {
-  res.json([
-    {
-      title: "Paneer Recipe",
-      category: "Dinner"
-    },
-    {
-      title: "Pasta",
-      category: "Lunch"
-    }
-  ]);
+  res.json(recipes);
 });
 
+// Search recipe
+router.get("/search", (req, res) => {
+  const query = req.query.name;
+
+  const filteredRecipes = recipes.filter(recipe =>
+    recipe.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  res.json(filteredRecipes);
+});
+
+// Single recipe
 router.get("/:id", (req, res) => {
-  res.json({
-    title: "Paneer Recipe",
-    description: "Delicious recipe"
-  });
+  const recipe = recipes.find(r => r.id == req.params.id);
+
+  res.json(recipe);
 });
 
 module.exports = router;
